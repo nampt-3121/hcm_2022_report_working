dabe9145351fdf16b71c9379568ac0d30f10c2b5
Nam's MacBook Pro <phamthanhnam@Namss-MacBook-Pro.local>
2022-07-23 00:22:14 +0700

    Fix delegate

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,7 +2,7 @@ class Relationship < ApplicationRecord
   belongs_to :user
   belongs_to :department
 
-  delegate :id, :full_name, to: :user
+  delegate :uid, :full_name, to: :user
 
   enum role_type: {employee: 0, manager: 1}
 end
